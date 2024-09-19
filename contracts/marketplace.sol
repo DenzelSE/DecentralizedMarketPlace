e02ed0ce2cbaf5c588b02553bddf72eab92de6d8
Denzel Uchiha <wtc@pop-os.localdomain>
2024-09-19 16:25:12 +0200

    buyProduct function added

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -1,9 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+interface IER20Token {
+    function transfer(address, uint256) external returns (bool);
+    function approve(address, uint256) external returns(bool);
+    function tranferFrom(address, address, uint256) external returns (bool);
+    function totalSupply() external view returns (uint256);
+    function balanceOf (address) external view returns (uint256);
+    function allowance (address, address) external view returns (uint256);
+    event Transfer (address indexed from, address indexed to, uint256 value);
+    event Approval (address indexed owner, address indexed spender, uint256 value);
+    
+}
+
 contract Marketplace {
     mapping (uint => Product) internal products;
     uint internal productlength = 0;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369b;
 
     // string internal product;
 
@@ -60,6 +73,18 @@ contract Marketplace {
         uint sold;
 
     }
+
+
+    function buyProduct(uint _index) public payable{
+        require(
+            IER20Token(cUsdTokenAddress).tranferFrom(msg.sender,
+            products[_index].owner,
+            products[_index].price),
+            "Transfer failed."
+        );
+        products[_index].sold++;
+    }
+    
     function getProductsLength() public view returns (uint){
         return (productlength);
     }
